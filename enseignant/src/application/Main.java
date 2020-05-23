909c1fd02724ec5c1fcda91ca0a764f5464021c7
Quentin <guez.quentin@gmail.com>
2020-05-23 20:35:53 +0200

    :sparkles: fin de la reconstitution du design

--- enseignant/src/application/Main.java
+++ enseignant/src/application/Main.java
@@ -2,16 +2,15 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../ressources/fxml/fenetre d'acceuil v2.fxml"));
 			Scene scene = new Scene(loader.load());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- etudiant/src/application/Main.java
+++ etudiant/src/application/Main.java
@@ -2,18 +2,16 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../../ressources/view/principal.fxml"));
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../ressources/fxml/principal.fxml"));
+			Scene scene = new Scene(loader.load());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
