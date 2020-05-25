82f715c23ced69e098e71785b18a4b5d34136ebf
Quentin <guez.quentin@gmail.com>
2020-05-25 19:44:15 +0200

    Fin de la tâche de l'importation de ressource

--- enseignant/src/controller/AccueilController.java
+++ enseignant/src/controller/AccueilController.java
@@ -1,13 +1,12 @@
 package controller;
 
-import java.io.IOException;
+import java.io.File;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class AccueilController {
@@ -25,20 +24,46 @@ public class AccueilController {
 	public void ouvrir(ActionEvent event) {
 		
 		MenuItem item = (MenuItem) event.getSource();
+		PrincipalController controller = null;
+		FXMLLoader loader;
+		Scene scene = null;
+		boolean valide = true;
 
 		try {
 
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../ressources/fxml/principal.fxml"));
-			Scene scene = new Scene(loader.load());
-			stage.setScene(scene);
-			stage.setTitle("Création d'un nouvel exercice");
-			stage.show();
+			loader = new FXMLLoader(getClass().getResource("../ressources/fxml/principal.fxml"));
+			scene = new Scene(loader.load());
+			controller = loader.getController();
+			controller.setStage(stage);
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
+		if (item.getText().equalsIgnoreCase("ouvrir")) {
+
+
+				FileChooser fileChooser = new FileChooser();
+				fileChooser.setTitle("Ouvrir un exercice");
+				fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Exercices (*.exo)", "*.exo"));
+				File file = fileChooser.showOpenDialog(stage);
+				controller.setExo(file);
+				System.out.println(file);
+
+				if (file == null)
+					valide = false;
+				else
+					valide = true;
+		}
+
+		if (valide) {
+
+			stage.setScene(scene);
+			stage.setTitle("Création d'un nouvel exercice");
+
+		}
+
 	}
 	
 }
--- enseignant/src/controller/PrincipalController.java
+++ enseignant/src/controller/PrincipalController.java
@@ -1,4 +1,77 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.File;
+import java.io.IOException;
+
 public class PrincipalController {
+
+    private File exo;
+    private Stage stage;
+
+    @FXML
+    private MediaView ressource;
+
+    public void importer (ActionEvent event) {
+
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Choisir une ressource");
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Ressource audio ou vidéo (*.mp4, *.mp3, *.wav)", "*.mp3", "*.mp4", "*.wav"));
+        File file = chooser.showOpenDialog(stage);
+        if (file != null) {
+            setMedia(file);
+        }else {
+            try {
+                Stage stage = new Stage();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../ressources/fxml/noRessource.fxml"));
+                stage.initStyle(StageStyle.UNDECORATED);
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.setAlwaysOnTop(true);
+
+                stage.setScene(new Scene(loader.load()));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public File getExo() {
+        return exo;
+    }
+
+    public void setExo(File exo) {
+        this.exo = exo;
+    }
+
+    public void setMedia (File file) {
+        MediaPlayer player = new MediaPlayer(new Media(file.toURI().toString()));
+        player.setAutoPlay(true);
+        player.setOnError(()->
+                System.out.println("media error"+player.getError().toString()));
+        ressource.setMediaPlayer(player);
+
+
+    }
+
 }
